304fb0b4c610e48be093a498a0f5ec89f14de586
苏平 王 <wsguochan007@aliyun.com>
2024-06-16 16:21:53 +0800

    2

--- cinget_single2.cpp
+++ cinget_single2.cpp
@@ -6,6 +6,29 @@ using namespace std;
 
 int main()
 {
+	char stri[10];
+	cout << "input string(10):";
+	cin.get(stri, 10);//cin.get()键盘输入字符数量超过数组大小10的限制，标志位没有影响。只是键盘剩余字符留在缓冲区。
+	                                             //但是，若是读取空行的话，则导致下次cin为false。
+	cout << "inputed string:" << stri << endl;
+	char ch;
+	while ((ch = cin.get()) != '\n')
+	{
+		cout << " ch:" << ch ;
+	}
+	return 0;
+	/*char stri[10];
+	cout << "input string(10):";
+	cin.getline(stri, 10);   //cin.getline()中键盘输入字符数量超过数组大小10的限制，则标志位cin.fail()设置为true，
+	                               //键盘剩余字符留在缓冲区，并且限制输入.可用cin.clear()重置标志位。
+	while (cin.fail())          
+	{
+		cin.clear();
+		int m = cin.get();
+		cout << "m is " << m<<" relevant char is " << char(m) << endl;
+	}
+	return 0;
+	*/
 	/*int num;
 	while ((num = cin.get()) != EOF)
 	{
