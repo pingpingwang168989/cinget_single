c077a94fc1f41222b4c4e363e80a14b5a2434e52
苏平 王 <wsguochan007@aliyun.com>
2024-06-16 13:24:11 +0800

    添加项目文件。

---
+++ cinget_single2.cpp
@@ -0,0 +1,49 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	/*int num;
+	while ((num = cin.get()) != EOF)
+	{
+		cout << "num:" << num << ' ';
+		cout.put(num);
+		cout << " ";
+
+	}
+	cout << "num:" << num << ' ';
+	// cout << "sizeof int:"<< sizeof(int)<< endl;
+	*/
+	cout << " input line string:";
+	char str[5];
+	cin.getline(str, 5);
+	cout << "inputed str:" << str << endl;
+	int inte2 = cin.get();
+	cout << "inter of intputed char:" << inte2 << endl;
+	// cout<<"ch:"<<ch<<endl;
+	int inte3 = cin.get();
+	cout << "inter of intputed char:" << inte3 << endl;
+	// cout<<"ch:"<<ch<<endl;
+	int inte = cin.get();
+	cout << "inter of intputed char:" << inte << endl;
+	// cout<<"ch:"<<ch<<endl;
+	/*const char* fn = "myfile2.txt";
+	ofstream  fout;
+	fout.open(fn);
+	if (!fout.is_open())
+	{
+		cout << "cannot open "
+			<< fn << "  bye" << endl;
+		exit(EXIT_FAILURE);
+	}
+	double object;
+	cout << "input focal length of double object:";
+	cin >> object;
+	double eps[4];
+	cout << "double object:" << object << endl;
+	//cout << "input focal length of "<<4<<" double eyes:"<<endl ;
+	*/
+}
